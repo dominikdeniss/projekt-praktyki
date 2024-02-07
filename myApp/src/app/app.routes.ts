ad33b7d6587c7d60582cb61929fa08d6744e00c2
dominik <dominik.denis@reesco.pl>
2024-02-07 14:42:51 +0100

    import and export new components

--- myApp/src/app/app.routes.ts
+++ myApp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GuideComponent } from './home/guide/guide.component';
+import { FlexboxComponent } from './home/layout/flexbox/flexbox.component';
+import { GridComponent } from './home/layout/grid/grid.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'guide', component: GuideComponent },
+  { path: 'layout/flexbox', component: FlexboxComponent },
+  { path: 'layout/grid', component: GridComponent },
+  { path: '', component: HomeComponent },
+];
