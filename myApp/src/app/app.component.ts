815d1a672691802a7d6584e5ff72624e53ed021f
dominik <dominik.denis@reesco.pl>
2024-02-12 10:56:45 +0100

    switched navigation from app.component to navigation-component

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,68 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuItem } from 'primeng/api';
-import { OnInit } from '@angular/core';
-import { MenubarModule } from 'primeng/menubar';
-
-
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, NavigationComponent],
 })
 export class AppComponent {
-  title = 'myApp'
-  items: MenuItem[] | undefined;
-
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'Siema tworzymy aplikacje',
-                icon: 'pi pi-fw pi-pencil',
-                routerLink: '/',
-                items: []
-            },
-            {
-                label: 'Guide',
-                icon: 'pi pi-fw pi-user',
-                routerLink: 'guide',
-                items: []
-                            },
-            {
-                label: 'Layout',
-                icon: 'pi pi-fw pi-calendar',
-                items: [
-                    {
-                        label: 'Grid',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: 'layout/grid',
-                        items: [
-                        ]
-                    },
-                    {
-                        label: 'Flexbox',
-                        icon: 'pi pi-fw pi-calendar-times',
-                        routerLink: 'layout/flexbox',
-                        items: [   
-                        ]
-                    },
-                ]
-
-                
-            },
-            {
-              label: 'Invoices',
-              icon: 'pi pi-fw pi-calendar-times',
-              items: [
-                {
-                  label: 'List',
-                  icon: 'pi pi-fw pi-calendar-times',
-                }
-              ]
-            }
-        ];
-    }
+  title = 'myApp';
 }
--- myApp/src/app/app.routes.ts
+++ myApp/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { GuideComponent } from './home/guide/guide.component';
 import { FlexboxComponent } from './home/layout/flexbox/flexbox.component';
 import { GridComponent } from './home/layout/grid/grid.component';
 import { LayoutComponent } from './home/layout/layout.component';
-
+import { NavigationComponent } from './navigation/navigation.component';
 
 export const routes: Routes = [
   { path: 'guide', component: GuideComponent },
@@ -12,5 +12,5 @@ export const routes: Routes = [
   { path: 'layout/flexbox', component: FlexboxComponent },
   { path: 'layout/grid', component: GridComponent },
   { path: 'layout', component: LayoutComponent },
-
+  { path: 'navigation', component: NavigationComponent },
 ];
