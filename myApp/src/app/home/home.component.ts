a42219d64b014d08eafd22a0c61f7d9cf0b2b61a
dominik <dominik.denis@reesco.pl>
2024-02-14 09:37:47 +0100

    changing guideList and adding scss style for home component and invoice component, adding div to invoice.component.html

--- myApp/src/app/home/home.component.ts
+++ myApp/src/app/home/home.component.ts
@@ -10,29 +10,52 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-    name: string = 'Jarek';
-    age: number = 30;
-    isMale: boolean = true;
-    actualDate: Date|undefined;
-    dateVisible: boolean = true;
-    guideList: string[]|["Zadanie1", "Zadanie2", "Zadanie3"];
+  name: string = 'Jarek';
+  age: number = 30;
+  isMale: boolean = true;
+  actualDate: Date | undefined;
+  dateVisible: boolean = true;
+  guideList:
+    | string[]
+    | [
+        'Przygotować miejsce do organizacji pracy',
+        'Setup WSL',
+        'Utworzyć repozytorium',
+        'Poznajcie NPM',
+        'Utowrzyć projekt Angular',
+        'Poznajcie Angular',
+        'Angular Routing',
+        'Zainstalujcie primeNG do waszego projektu Angular',
+        'SCSS/Sass',
+        'Poznajcie flexbox i grid',
+        'Stwórzmy nawigację',
+        'TypeScript i Angular'
+      ];
 
-    ngOnInit(): void{
-      this.guideList.push("Zadanie1");
-      this.guideList.push("Zadanie2");
-      this.guideList.push("Zadanie3");
+  ngOnInit(): void {
+    this.guideList.push('Przygotować miejsce do organizacji pracy');
+    this.guideList.push('Setup WSL');
+    this.guideList.push('Utworzyć repozytorium');
+    this.guideList.push('Poznajcie NPM');
+    this.guideList.push('Utowrzyć projekt Angular');
+    this.guideList.push('Poznajcie Angular');
+    this.guideList.push('Angular Routing');
+    this.guideList.push('Zainstalujcie primeNG do waszego projektu Angular');
+    this.guideList.push('SCSS/Sass');
+    this.guideList.push('Poznajcie flexbox i grid');
+    this.guideList.push('Stwórzmy nawigację');
+    this.guideList.push('TypeScript i Angular');
+  }
 
-    }
+  constructor() {
+    this.actualDate = new Date();
+    this.guideList = [];
+  }
 
-    constructor(){
-      this.actualDate = new Date();
-      this.guideList = [];
-    }
-
-    showDateTime(){
-      this.actualDate = new Date();
-    }
-    toggleDateVisibility(){
-      this.dateVisible = !this.dateVisible;
-    }
+  showDateTime() {
+    this.actualDate = new Date();
+  }
+  toggleDateVisibility() {
+    this.dateVisible = !this.dateVisible;
+  }
 }
